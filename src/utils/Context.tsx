4c5a221a5fd31159cf50e607f3789c9887cf103f
Michael Czuba <czuba.mich@gmail.com>
2021-10-31 23:23:00 +0100

    Fix AuthContext

--- src/utils/Context.tsx
+++ src/utils/Context.tsx
@@ -1,26 +1,3 @@
 import React from 'react'
 
-import { Text, View } from 'react-native'
-
-export default AuthContext = () => {
-    constructor() {
-        [isLoading, setIsLoading] = React.useState(true)
-        [userToken, setUserToken] = React.useState(null)
-    }
-
-    readonly context = React.createContext(null);
-    readonly authContext = React.useMemo(() => ({
-        signIn: () => {
-          setUserToken("token")
-          setIsLoading(false)
-        },
-        signUp: () => {
-          setUserToken("token")
-          setIsLoading(false)
-        },
-        signOut: () => {
-          setUserToken(null)
-          setIsLoading(false)
-        },
-      }), null);   
-}
+export const AuthContext = React.createContext(null);
